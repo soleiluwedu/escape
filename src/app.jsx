f66b93641fc38c235f7e174cc1e0f761d0d25e3d
Thai-Duong Nguyen <soleil@uw.edu>
2017-05-21 17:50:15 -0700

    Worker is now terminated after failing to check soon after mission briefing.

--- src/app.jsx
+++ src/app.jsx
@@ -5,99 +5,128 @@ import Button from './button.jsx';
 import Output from './output.jsx';
 
 class App extends Component {
+
+  // Main constructor method.
   constructor(props) {
+
+    // Included for good practice, not out of necessity.
     super(props);
 
+    // Standard React this.state. Contains information collected from or outputted to DOM.
     this.state = {
+
       // Value of editor textarea.
       editorContent: '',
 
       // Value of Output Component's div.
       outputContent: '',
-    }
 
-    // this.shadowState contains application state that is not passed down as props.
+    } // End this.state object.
+
+    // this.shadowState contains state secrets not shared elsewhere in the application.
     this.shadowState = {
-      // Next worker ID that will be used.
-      nextID: false,
 
-      // Object with worker ID keys and boolean values to track if worker is still working.
-      workerStatuses: {},
+      // Asset is a web worker to be used to eval code to help keep the main script safe from errors.
+      asset: null,
 
-      // Number tracks total number of workers still active.
-      totalActive: 0
-    }
+      // Boolean to indicate if there is a deployed asset.
+      assetDeployed: false,
 
-    // Bind functions that will be passed to children components.
+      // setTimeout ID of function to kill asset if asset has not checked in recently.
+      assassinID: null,
+
+      // Assets must check in after 'deadline' number of milliseconds, or be killed.
+      deadline: 1000
+
+    } // End this.shadowState object.
+
+    // Bind methods that will be passed to children components.
+
+    // Keep track of editor text. May come in handy if code-sharing funtionality is added in the future.
     this.onchange = this.onchange.bind(this);
+
+    // When "Run Code" button is clicked, clear Output Component and brief asset.
     this.runcode = this.runcode.bind(this);
+
+    // Render output to Output Component.
     this.renderOutput = this.renderOutput.bind(this);
-  }
 
-  // Creates worker to eval code without risk of crashing main script.
-  evalWithWorker(commandObj) {
-
-    // Unique ID to track worker status. Increment main count after saving ID.
-    const id = this.shadowState.nextID++;
-    this.shadowState.totalActive++;
-
-    // Instantiate worker.
-    const evalWorker = new Worker('./src/worker.js');
-
-    // Worker is working.
-    this.shadowState.workerStatuses[id] = true;
-
-    // Update output upon worker response.
-    evalWorker.addEventListener('message', response => {
-      switch (response.data.action) {
-        // Render one more line of output.
-        case 'addline': return this.renderOutput(this.state.outputContent + response.data.content);
-
-        // Spawn a new worker once. Used for setTimeout.
-        case 'spawn': return this.evalWithWorker({ command: 'eval', payload: response.data.content });
-
-        // Spawn a new worker continuously with setInterval.
-        case 'spawnInterval': return this.evalWithWorker({ command: 'setInterval', func: response.data.func, wait: response.data.wait });
-        
-        // Use setTimeout to kill a workers so we do not build up total number of workers. Omit break or return to fall to next line.
-        case 'finishSetInterval': setTimeout(() => evalWorker.terminate(), response.data.wait);
-        
-        // Worker 'finish' and 'finishSetInterval' responses BOTH trigger shadowState updates.
-        case 'finish':
-          this.shadowState.workerStatuses[id] = false;
-          this.shadowState.totalActive--;
-          return;
-      }
-    });
-
-    // Passing command object to worker to eval code.
-    evalWorker.postMessage(commandObj);
-
-    // Kill worker after half a second if it is still working.
-    setTimeout(() => {
-      if (this.shadowState.workerStatuses[id]) {
-        evalWorker.terminate();
-        this.shadowState.workerStatuses[id] = false;
-        this.shadowState.totalActive--;
-        this.renderOutput(this.state.outputContent + 'Error: Code timed out.\n');
-      }
-    }, 500);
-  }
-
-  // Render worker output (return value of evalled code) to DOM.
-  renderOutput(output) { this.setState({ outputContent: output }); }
+  } // End main constructor method.
 
   // Keep track of editor text. May come in handy if code-sharing funtionality is added in the future.
   onchange(e) { this.setState({ editorContent: e.target.value }); }
 
-  // When user clicks "Run Code" button.
+  // When "Run Code" button is clicked, clear Output Component and brief asset.
   runcode() {
-    if (this.shadowState.totalActive) return this.renderOutput('Error: Previous Run Code command still executing.');
-    this.renderOutput('');
-    this.evalWithWorker({ command: 'eval', payload: this.state.editorContent });
-  }
 
-  // Render code editor and console output.
+    // Clear Output Component's content.
+    this.renderOutput('');
+
+    // Send Editor Component's content to asset as mission briefing.
+    this.briefAsset(this.state.editorContent);
+
+  } // End runcode method.
+
+  // Render output to Output Component.
+  renderOutput(output) { this.setState({ outputContent: output }); }
+
+  // Deploy asset for initial mission and stay deployed for asynchronous mission creep.
+  deployAsset() {
+
+    // Activate asset.
+    this.shadowState.asset = new Worker('./src/worker.js');
+
+    // Update record to indicate that an asset is currently deployed.
+    this.shadowState.assetDeployed = true;
+
+    // Protocol for receipt of report from asset.
+    this.shadowState.asset.onmessage = report => {
+
+      // Cancel hit.
+      clearTimeout(this.shadowState.assassinID);
+
+      // Deliver report from asset.
+      return this.renderOutput(this.state.outputContent + report.data);
+
+    } // End this.shadowState.asset.onmessage method.
+
+  } // End deployAsset method.
+
+  // Brief asset on mission. Deploy new asset first if none are active.
+  briefAsset(code) {
+
+    // If no assets are currently deployed, deploy a new asset.
+    if (!this.shadowState.assetDeployed) this.deployAsset();
+
+    // Send mission briefing to asset.
+    this.shadowState.asset.postMessage(code);
+
+    // Put out a hit on the asset that will be cancelled if asset reports back in time.
+    this.assassinStandby();
+
+  } // End briefAsset method.
+
+  // Activate assassin to eliminate asset upon lack of timely report.
+  assassinStandby() {
+
+    // Save setTimeout ID of assassin to allow cancellation.
+    this.shadowState.assassinID = setTimeout(() => {
+
+      // Assassinate asset.
+      this.shadowState.asset.terminate();
+
+      // Update record to indicate no assets currently deployed.
+      this.shadowState.assetDeployed = false;
+
+      // Put out a public statement covering up the incident.
+      this.renderOutput('Code timed out.');
+
+      // setTimeout delay: Asset must report back in time or be eliminated.
+    }, this.shadowState.deadline);
+
+  } // End assassinStandby method.
+
+  // Render code editor (Editor Component) and console output (Output Component).
   render() {
     return (
       <div id="app">
@@ -107,7 +136,8 @@ class App extends Component {
         <Output content={this.state.outputContent} outputId="output" />
       </div>
     )
-  }
-}
+  } // End render method.
+} // End App Component class.
 
+// Targets div on index.html with id of 'x' as entry point for React App Component.
 render(<App />, document.getElementById('x'));
--- src/worker.js
+++ src/worker.js
@@ -1,18 +1,27 @@
-// Stringify data. Function is available on GitHub as a standalone repo.
+// Stringify data to optimize inspection of JavaScript expressions.
+// Function is available on GitHub as a standalone repo.
 function unlace(data) {
 
   // These data types do not have toString() functionality.
   switch (data) {
-    case undefined: return 'undefined';
+
+    // Return string 'null' to prevent confusing output of actual null value.
     case null: return 'null';
-  }
+
+    // Return string 'undefined' to prevent confusing output of actual undefined value.
+    case undefined: return 'undefined';
+  
+  } // End switch statement checking for null or undefined values.
 
   // Conditionally stringify according to data type.
   switch (typeof data) {
 
+    // Objects will be stringified recursively.
     case 'object':
+  
       // Recursively stringify any nested Array instances.
       if (Array.isArray(data)) return `[${data.map(e => unlace(e)).join(', ')}]`;
+  
       // Recursively stringify any nested Object literals.
       else return `{ ${Object.keys(data).map(key => key + ": " + unlace(data[key])).join(', ')} }`;
 
@@ -22,55 +31,31 @@ function unlace(data) {
     // All others will be stringified with toString().
     default: return data.toString();
 
-  }
+  } // End switch statement checking data type.
 
-}
+} // End unlace function.
 
 // On receipt of data, eval code and send back one string containing all console.logs.
-self.addEventListener("message", e => {
-
-  // Save original functions.
-  console.origLog = console.log;
-  origSetTimeout = setTimeout;
-  origSetInterval = setInterval;
+self.onmessage = briefing => {
 
   // One string to contain all console.logs in code.
   let log = '';
 
-  // Monkeypatch console.log to collect arguments and post message containing all arguments.
+  // Monkeypatch console.log to simply collect arguments into 'log' variable.
   console.log = (...args) => log += args.map(e => unlace(e)).join(' ') + '\n';
 
-  // Monkeypatch setTimeout to call another web worker to eval callback.
-  setTimeout = (func, wait) => origSetTimeout(() => e.srcElement.postMessage({ action: 'spawn', content: `(${func})()` }), wait);
+  // Eval code.
+  try { eval(briefing.data); }
 
-  // Monkeypatch setInterval to use setTimeout call another web worker to eval callback. This will enforce safety checks on each execution.
-  setInterval = (func, wait) => origSetTimeout(() => e.srcElement.postMessage({ action: 'spawnInterval', func: func.toString(), wait }), wait);
+  // Catch and report error.
+  catch (err) { self.postMessage(`Error: ${err.message}\n`); }
 
-  try {
-    switch (e.data.command) {
-      // Eval code.
-      case 'eval': eval(e.data.payload); break;
+  // Post back console.log output if any exists.
+  finally { if (log.length) self.postMessage(log); }
 
-      // Eval a given function and use the monkeypatched setInterval function to repeact functionality.
-      case 'setInterval':
-        eval(`(${e.data.func})()`);
-        var id = setInterval(e.data.func, e.data.wait);
-        break;
-    }
+  // In between messages received (which occur on Run Code command), monkeypatch console.log
+  // to immediately post messages. Otherwise async operations cannot console.log.
+  // Trade-off is that infinite loops inside async operations will crash page.
+  console.log = (...args) => self.postMessage(args.map(e => unlace(e)).join(' ') + '\n');
 
-    // Post all console.log content as message.
-    e.srcElement.postMessage({ action: 'addline', content: log });
-  }
-
-  // Catch errors.
-  catch (err) { e.srcElement.postMessage({ action: 'addline', content: `Error: ${err.message}\n` }); }
-
-  // Notify main script that worker is finished.
-  finally {
-    switch (e.data.command) {
-      case 'setInterval': return e.srcElement.postMessage({ action: 'finishSetInterval', id, wait: e.data.wait });
-      default: return e.srcElement.postMessage({ action: 'finish' });
-    }
-  }
-
-});
+};
